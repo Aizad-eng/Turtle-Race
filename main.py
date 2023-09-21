3940660574780fd04fc4a8a1552df6e3a041819d
Aizad-eng <dxuxama@gmail.com>
2023-09-21 12:22:41 +0500

    Updated the race mechansim to look more smooth

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Turtle Race
-
+import time
 from turtle import Turtle, Screen
 import random
 
 screen = Screen()
+screen.tracer(0)
 screen.bgcolor("black")
 colors  = ["red","blue","green","purple","orange","violet"]
 choice = screen.textinput("Decide winner: ", "Which color turtle will win? ")
@@ -21,30 +22,31 @@ for i in range(6):
 print(screen.screensize())
 
 def race_start():
-    ycor = -80
+    ycor = -180
     for turtle in turtles:
         turtle.penup()
-        turtle.shapesize(3,3)
+        turtle.shapesize(2,2)
         turtle.goto(-450, ycor)
         ycor += 80
 
 race_start()
+screen.update()
 race_not_finished = True
 
 while race_not_finished:
+    screen.update()
     for turtle in turtles:
+        time.sleep(0.01)
+        screen.update()
         for i in range(1):
-            turtle.speed(random.randint(1, 20))
             turtle.forward(random.randint(1, 30))
             if turtle.xcor()>=430:
-                index_of_winner_turtle = turtles.index(turtle)
-                winner_turtle_color = colors[index_of_winner_turtle]
-                print(f"{winner_turtle_color} Won!")
+                winner_turtle_color = turtle.pencolor()
                 race_not_finished = False
-if choice.lower() == winner_turtle_color:
-    print("You won the bet")
-else:
-    print("You lost the bet")
+                if choice.lower() == winner_turtle_color:
+                    print("You won the bet")
+                else:
+                    print(f"You lost the bet, {winner_turtle_color} won!")
 
 
 
